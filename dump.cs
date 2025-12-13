cb09578396cb93f06bf056efb1300406883cc28b
Castform <35473641+ShinyCastform@users.noreply.github.com>
2025-12-13 16:05:11 -0400

    Update dump.cs

--- dump.cs
+++ dump.cs
@@ -1,5 +1,14 @@
-//The object responsible for audio playback
-public AudioSource audioSource;
- 
-//Sound file containing the sound of damage
-public AudioClip damageSound;
+public void TakeDamage(int damage)
+{
+ health -= damage;
+
+      if(health > 0)
+      {
+          audioSource.PlayOneShot(damageSound);
+      }
+      else
+      {
+          int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+          SceneManager.LoadScene(sceneIndex);
+      }
+}
